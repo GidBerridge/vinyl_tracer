e452cf32e07f4756a282658ff8564c9e31871a15
Gid <gid@Gids-iMac.local>
2023-01-19 20:24:56 +0000

    album_text

--- app/javascript/packs/album.js
+++ app/javascript/packs/album.js
@@ -17,19 +17,6 @@ const album = () => {
         processId(data)
       })
 
-    // $.ajax({
-    //   url: 'https://en.wikipedia.org/w/api.php',
-    //   data: {
-    //     action: 'query',
-    //     prop: 'extracts',
-    //     list: 'search',
-    //     srsearch: album_name_request.innerText,
-    //     format: 'json',
-    //   },
-    //   dataType: 'jsonp',
-    //   success: processId,
-    // })
-
     let pageId
 
     function processId(data) {
@@ -46,36 +33,23 @@ const album = () => {
           return resp.json()
         })
         .then(function (data) {
-          console.log(data)
           processResult(data)
         })
-      // $.ajax({
-      //   url: 'https://en.wikipedia.org/w/api.php',
-      //   data: {
-      //     action: 'query',
-      //     prop: 'extracts',
-      //     redirects: 1,
-      //     exsentences: 3,
-      //     exintro: true,
-      //     explaintext: true,
-      //     pageids: pageId,
-      //     format: 'json',
-      //   },
-      //   dataType: 'jsonp',
-      //   success: processResult,
-      // })
 
-      function processResult(data) {
+      const processResult = (data) => {
         const pages = data.query.pages
         const pages_inner = Object.keys(pages)[0]
         const text = pages[pages_inner].extract
         const album_trivia = document.getElementById('album_trivia')
-        album_trivia.innerText = text
+        const longText = '....... \n\n...see wikipedia for more details.'
+        if (text.length >= 600) {
+          const trimmedText = text.replace(/^(.{520}[^\s]*).*/, '$1')
+          console.log(trimmedText)
+          album_trivia.innerText = trimmedText + longText
+        } else album_trivia.innerText = text
       }
     }
   }
 }
 
 export { album }
-
-//https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=rollingstones
